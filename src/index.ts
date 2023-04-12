b97ea80b0de9e968610e6ca108478c762fd0f143
subham-padhi-01 <subham@inito.com>
2023-04-13 04:34:14 +0530

    mission-log: forward requests to rails server acting as a proxy

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+function proxyRequest(proxyReq: any) {
+  console.log(proxyReq);
+}
+
+function proxyResponse(proxyRes: any) {
+console.log(proxyRes);
+}
+
+const railsProxy = createProxyMiddleware({
+  target: 'http://localhost:3000',
+  changeOrigin: true,
+  onProxyReq: proxyRequest,
+  onProxyRes: proxyResponse
+});
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, world!");
-});
+app.use('/*', railsProxy);
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello, world!");
+// });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(3002, () => {
+  console.log("Server is running on http://localhost:3002");
 });
